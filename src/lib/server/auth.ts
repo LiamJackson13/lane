b4d3b57ccb3ed3d732c3efff9ff2a5083c765578
Flow <jac0031@willihigh.vic.edu.au>
2026-04-05 15:54:52 +1000

    added organisation integration and automatic org creation on signup

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,6 +4,7 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
+import { organization } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
@@ -11,6 +12,25 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, { provider: 'pg' }),
 	emailAndPassword: { enabled: true },
 	plugins: [
+		organization(),
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
-	]
+	],
+	databaseHooks: {
+		user: {
+			create: {
+				after: async (user, ctx) => {
+					if (ctx?.path !== '/sign-up/email') return;
+
+					await auth.api.createOrganization({
+						body: {
+							name: user.name + "'s Organization",
+							slug: user.name.toLowerCase().replace(/\s+/g, '-') + '-' + user.id.slice(0, 6),
+							userId: user.id,
+							keepCurrentActiveOrganization: false
+						}
+					});
+				}
+			}
+		}
+	}
 });
