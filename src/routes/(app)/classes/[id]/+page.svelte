4827dc6822a1c5fe5b83eddd736a41d87236122e
Liam Jackson <flow.dev30@gmail.com>
2026-04-08 22:37:01 +1000

    testing lazygit
    this is a commit to test lazygit

--- src/routes/(app)/classes/[id]/+page.svelte
+++ src/routes/(app)/classes/[id]/+page.svelte
@@ -1,9 +1,4 @@
-<script lang="ts">
-	import type { PageProps } from './$types';
-	import { getClassById } from '../classes.remote';
-	let { params }: PageProps = $props();
-	const schoolClass = $derived(await getClassById(params.id));
-</script>
+<script lang="ts" ✂prettier:content✂="CglpbXBvcnQgdHlwZSB7IFBhZ2VQcm9wcyB9IGZyb20gJy4vJHR5cGVzJzsKCWltcG9ydCB7IGdldENsYXNzQnlJZCB9IGZyb20gJy4uL2NsYXNzZXMucmVtb3RlJzsKCWxldCB7IHBhcmFtcyB9OiBQYWdlUHJvcHMgPSAkcHJvcHMoKTsKCWNvbnN0IHNjaG9vbENsYXNzID0gJGRlcml2ZWQoYXdhaXQgZ2V0Q2xhc3NCeUlkKHBhcmFtcy5pZCkpOwo=">{}</script>
 
 {#if schoolClass}
 	<p>class detail page for {schoolClass.name}</p>
